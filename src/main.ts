e2ac26be2a16261d2f2023bbabcc75ba9c01b7c2
늘 재밌는 세상, 고뽀고 <59677526+gofogo2@users.noreply.github.com>
2024-01-03 10:11:04 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   setupSwagger(app);
-  await app.listen(4000);
+  await app.listen(5000);
 }
 bootstrap();
